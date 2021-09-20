3946f28d97eb839dfc3e6ebe5eb2fae9e325524a
Ashley U <ashley.umetsu@gmail.com>
2021-09-20 11:55:17 -0500

    getting rid of sqlite

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 
-const {sequelize} = require('./db');
-const {Sauce} = require('./models');
+// const {sequelize} = require('./db');
+// const {Sauce} = require('./models');
 
-const seed = require('./seed')
+// const seed = require('./seed')
 
 const PORT = process.env.PORT || 3000;
 
@@ -21,19 +21,19 @@ app.use(express.static('public'));
 
 
 
-seed();
+// seed();
 
-app.get('/sauces', async (req, res) => {
-    const sauces= await Sauce.findAll()
-    res.json(sauces)
-})
+// app.get('/sauces', async (req, res) => {
+//     const sauces= await Sauce.findAll()
+//     res.json(sauces)
+// })
 
-app.get('/sauces/:id', async (req, res) => {
-    const sauce = await Sauce.findByPk(req.params.id)
-    res.json({sauce})
-})
+// app.get('/sauces/:id', async (req, res) => {
+//     const sauce = await Sauce.findByPk(req.params.id)
+//     res.json({sauce})
+// })
 
 app.listen(PORT, () => {
-    sequelize.sync({force: true});
+    // sequelize.sync({force: true});
     console.log(`Your server is running on http://localhost:${PORT}`);
 })
